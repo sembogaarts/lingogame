122d007566f69c2c2eaeed6a1f25c8b74c3b9118
Sem Bogaarts <sembogaarts@gmail.com>
2020-06-14 22:09:35 +0200

    Added more tests

--- tests/Unit/BoardHelperTest.php
+++ tests/Unit/BoardHelperTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Attempt;
+use App\Enums\LetterStatus;
 use App\Game;
 use App\Helpers\BoardHelper;
 use App\Helpers\GameHelper;
@@ -18,7 +19,7 @@ class BoardHelperTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * Test if the class gives the right first letter
+     * Test if the boards gives an expected input
      *
      * @return void
      */
@@ -54,6 +55,66 @@ class BoardHelperTest extends TestCase
         ];
 
         $this->assertTrue($input === $expectedInput);
+    }
+
+    /**
+     * Test if the rows match
+     */
+    public function testRows() {
+        $user = factory(User::class)->create();
+
+        $game = factory(Game::class)->create([
+            'user_id' => $user->id,
+            'word_length' => 5
+        ]);
+
+        $round = factory(Round::class)->create([
+            'game_id' => $game->id,
+            'word' => 'hallo'
+        ]);
+
+        $attempts = [];
+
+        $attempts[] = factory(Attempt::class)->create([
+            'round_id' => $round->id,
+            'word' => 'holaa'
+        ]);
+
+        $attempts[] = factory(Attempt::class)->create([
+            'round_id' => $round->id,
+            'word' => 'holla'
+        ]);
+
+        $gameHelper = new BoardHelper($game, $round, $attempts);
+
+        $score = $gameHelper->render();
+
+        $rows = $score->getRows();
+
+        $expectedRows = [
+            [
+                'word' => ['h', 'o', 'l', 'a', 'a'],
+                'status' => [
+                    LetterStatus::Correct,
+                    LetterStatus::WrongPosition,
+                    LetterStatus::Correct,
+                    LetterStatus::WrongPosition,
+                    LetterStatus::Incorrect
+                ]
+            ],
+            [
+                'word' => ['h', 'o', 'l', 'l', 'a'],
+                'status' => [
+                    LetterStatus::Correct,
+                    LetterStatus::WrongPosition,
+                    LetterStatus::Correct,
+                    LetterStatus::Correct,
+                    LetterStatus::WrongPosition
+                ]
+            ]
+        ];
+
+        $this->assertTrue($rows === $expectedRows);
     }
 
 }
