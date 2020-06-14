c6f6042026aad24edde09ca74726f3cf041963ba
Sem Bogaarts <sembogaarts@gmail.com>
2020-06-14 22:13:22 +0200

    Added docs

--- app/Helpers/GameHelper.php
+++ app/Helpers/GameHelper.php
@@ -13,6 +13,10 @@ class GameHelper
     private $_game;
     private $_rounds;
 
+    /**
+     * GameHelper constructor.
+     * @param $game
+     */
     public function __construct($game)
     {
         // Set the game
@@ -21,18 +25,35 @@ class GameHelper
         $this->getRounds();
     }
 
+    /**
+     * Set the rounds for the game local
+     */
     private function getRounds() {
         $this->_rounds = Round::where('game_id', $this->_game->id)->get();
     }
 
+    /**
+     * Returns the length of the words
+     * @return integer
+     */
     public function getWordLength() {
         return $this->_game->word_length;
     }
 
+    /**
+     * Returns the first letter of the word in the current round
+     * @return string
+     */
     public function getFirstLetterOfWord() {
         return $this->currentRound()->word[0];
     }
 
+    /**
+     * Creates a new game for the user and creates a new GameHelper class
+     * @param $user
+     * @param $wordLength
+     * @return GameHelper
+     */
     public static function create($user, $wordLength)
     {
 
@@ -45,15 +66,27 @@ class GameHelper
 
     }
 
+    /**
+     * Gets the current round
+     * @return Round
+     */
     public function currentRound() {
         $lastIndex = array_key_last($this->_rounds->toArray());
         return last($this->_rounds)[$lastIndex];
     }
 
+    /**
+     * Returns all the attempts for the current round
+     * @return array
+     */
     public function attemptsForCurrentRound() {
         return $this->currentRound()->attempts;
     }
 
+    /**
+     * Creates a new round for the game
+     * @return $this
+     */
     public function newRound()
     {
         $word = WordHelper::randomWord($this->_game->word_length);
@@ -67,6 +100,11 @@ class GameHelper
 
     }
 
+    /**
+     * Makes an attempt for the current round
+     * @param $word
+     * @return mixed
+     */
     public function makeAttempt($word) {
         $attempt = Attempt::create([
             'round_id' => $this->currentRound()->id,
@@ -75,16 +113,29 @@ class GameHelper
         return $attempt;
     }
 
+    /**
+     * Checks if the attempt is valid
+     * @param Attempt $attempt
+     * @return bool
+     */
     public function checkAttempt(Attempt $attempt) {
         return $this->currentRound()->word === $attempt->word;
     }
 
+    /**
+     * @return $this
+     */
     public function stop() {
         $this->_game->finished_at = Carbon::now();
         $this->_game->score = $this->calculateScore();
         $this->_game->save();
+        return $this;
     }
 
+    /**
+     * Calculates the score for the current game
+     * @return int
+     */
     public function calculateScore() {
         $points = 0;
         foreach($this->_rounds as $round) {
@@ -95,6 +146,11 @@ class GameHelper
         return $points;
     }
 
+    /**
+     * Finish the current roundgi
+     * @param bool $success
+     * @return Round
+     */
     public function finishRound(bool $success) {
         $round = $this->currentRound();
         $round->success = $success;
